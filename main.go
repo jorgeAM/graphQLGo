a0d418d6500aed241acb0618ff60013f0947a8fd
Jorge L <jorge.alfmur@gmail.com>
2020-06-10 14:46:20 -0500

    install all dependecies what we're gonna use and create docker-compose to connect with database in this case would be postgres

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	_ "github.com/joho/godotenv/autoload"
 	"github.com/jorgeAM/basicGraphql/generated"
 	"github.com/jorgeAM/basicGraphql/resolver"
 )
@@ -15,15 +16,14 @@ const defaultPort = "8080"
 
 func main() {
 	port := os.Getenv("PORT")
+
 	if port == "" {
 		port = defaultPort
 	}
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.Resolver{}}))
-
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
-
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
